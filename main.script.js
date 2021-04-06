2244a53cd14749281a7f519cf49b3e5d7ddc3f88
spike <>
2021-04-06 22:37:06 +0200

    test script

--- main.script.js
+++ main.script.js
@@ -1,3 +1,15 @@
+let helloWorldPopup;
+
+// Open the popup when we enter a given zone
+helloWorldPopup = WA.openPopup("popupRectangle", 'Hello world!', {
+        label: "Close",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    })
+
 WA.openTab('example.com');
 WA.onEnterZone('npcZone', () => {
     WA.sendChatMessage("Hello, how can I help you?", 'Mr.Teddy');
