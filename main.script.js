fce5bef1dbfdcf32d2750488ca609f9f10284e55
spike <>
2021-04-06 22:07:28 +0200

    script update

--- main.script.js
+++ main.script.js
@@ -1,8 +1,8 @@
 WA.onEnterZone('npcZone', () => {
     WA.sendChatMessage("Hello, how can I help you?", 'Mr.Teddy');
-}
+});
 WA.onLeaveZone('npcZone', () => {
     WA.sendChatMessage("GoodBye", 'Mr.Teddy');
-}
+});
 WA.sendChatMessage("test", 'test');
 
