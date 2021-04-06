5f21a9bb86a475f51ac3e3ab6f762f5a6dbfb933
spike <>
2021-04-06 18:45:44 +0200

    adding Mr.Teddy NPC

---
+++ main.script.js
@@ -0,0 +1,6 @@
+onEnterZone('npcZone', () => {
+    WA.sendChatMessage("Hello, how can I help you?", 'Mr.Teddy');
+}
+onLeaveZone('npcZone', () => {
+    WA.sendChatMessage("GoodBye", 'Mr.Teddy');
+}
