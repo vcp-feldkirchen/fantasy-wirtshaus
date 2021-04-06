1d5cd92049c0f93f9a67b8470f23af9178e28858
spike <>
2021-04-06 19:00:04 +0200

    testing scripting

--- main.script.js
+++ main.script.js
@@ -1,7 +1,8 @@
-onEnterZone('npcZone', () => {
+WA.onEnterZone('npcZone', () => {
     WA.sendChatMessage("Hello, how can I help you?", 'Mr.Teddy');
 }
-onLeaveZone('npcZone', () => {
+WA.onLeaveZone('npcZone', () => {
     WA.sendChatMessage("GoodBye", 'Mr.Teddy');
 }
 WA.sendChatMessage("test", 'test');
+
